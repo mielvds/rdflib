8c9637b7224c152f47c25f7493c3e9c95f79a655
Miel Vander Sande <miel.vandersande@meemoo.be>
2026-08-18 14:06:57 +0200

    Do not print datatype when serializing xsd:string literals.

--- rdflib/plugins/serializers/nt.py
+++ rdflib/plugins/serializers/nt.py
@@ -5,6 +5,7 @@ import warnings
 from typing import IO, TYPE_CHECKING, Any, Optional, Tuple, Union
 
 from rdflib.graph import Graph
+from rdflib.namespace._XSD import XSD
 from rdflib.serializer import Serializer
 from rdflib.term import Literal
 
@@ -75,7 +76,7 @@ def _quoteLiteral(l_: Literal) -> str:  # noqa: N802
         if l_.datatype:
             raise Exception("Literal has datatype AND language!")
         return "%s@%s" % (encoded, l_.language)
-    elif l_.datatype:
+    elif l_.datatype and l_.datatype != XSD.string:
         return "%s^^<%s>" % (encoded, l_.datatype)
     else:
         return "%s" % encoded
